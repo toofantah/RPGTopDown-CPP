cbe10f0735c95494b897dcd91b3fa976975aa448
Toofan Tahmouresi <toofan.tah@gmail.com>
2023-02-26 15:10:08 +0300

    refactor movements and animation

--- main.cpp
+++ main.cpp
@@ -11,15 +11,26 @@ int main()
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
     Vector2 mapPos{0.0,0.0};
     float speed{4.0};
-
-    Texture2D player = LoadTexture("characters/knight_idle_spritesheet.png");
+    
+    // player
+    Texture2D player;
+    Texture2D playerIdle = LoadTexture("characters/knight_idle_spritesheet.png");
+    Texture2D playerRun = LoadTexture("characters/knight_run_spritesheet.png");
     Vector2 playerPos{
-        (float)windowWidth/2 - 4.0f * (0.5f * (float)player.width/6.0f),
-        (float)windowHeight/2 - 4.0f * (0.5f * (float)player.height/6.0f),
+        (float)windowWidth/2 - 4.0f * (0.5f * (float)playerIdle.width/6.0f),
+        (float)windowHeight/2 - 4.0f * (0.5f * (float)playerIdle.height/6.0f),
     };
+    
+    float playerDirX{1.f};
+    // anim vars
+    float runningTime{};
+    int frame{};
+    int maxFrames{6};
+    float frameRate{1.f / 12.f};
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
+        float dT = GetFrameTime();
         BeginDrawing();
         ClearBackground(BLACK);
         // float dT = GetFrameTime();
@@ -30,12 +41,26 @@ int main()
         if(IsKeyDown(KEY_W)) direction.y-= 1.0;
         if(IsKeyDown(KEY_S)) direction.y+= 1.0;
 
-        if(Vector2Length(direction) != 0)
-          mapPos = Vector2Subtract(mapPos,Vector2Scale(Vector2Normalize(direction), speed));
+        if(Vector2Length(direction) != 0){
+            mapPos = Vector2Subtract(mapPos,Vector2Scale(Vector2Normalize(direction), speed));
+            direction.x < 0.f ? playerDirX = -1 : playerDirX = 1;
+            player = playerRun;
+        }else{
+            player = playerIdle;
+        }
+          
         //Draw World
         DrawTextureEx(map,mapPos,0,4,WHITE);
+
+        //Update Animation Frames
+        runningTime += dT;
+        if(runningTime >= frameRate){
+            frame++;
+            runningTime = 0.f;
+            if(frame > maxFrames) frame = 0;
+        }
         //Draw player
-        Rectangle source {0.f,0.f, (float)player.width / 6.f, (float)player.height};
+        Rectangle source { frame * (float)player.width / 6.f,0.f, playerDirX * (float)player.width / 6.f, (float)player.height};
         Rectangle dest {playerPos.x, playerPos.y, 4.0f * (float) player.width / 6.f, 4.f * (float)player.height};
         DrawTexturePro(player, source, dest, Vector2{}, 0.0f, WHITE);
 
